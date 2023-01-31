5e1618285ba9830da565e4dfaf0a17e858a8a8a6
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-31 22:11:26 +0900

    jest.config: add comments

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,16 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
 
+  /* Jest がファイルを検索するために使用するディレクトリパス*/
   roots: ["<rootDir>/src", "<rootDir>/tests"],
+
+  /* テストファイル名にマッチする正規表現文字列の配列 */
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
+
+  /* 正規表現でマッチしたファイルに対してts-jestを使うように指示 */
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
